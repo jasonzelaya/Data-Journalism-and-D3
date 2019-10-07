296c3d6d73ffe1874c66e14059be63ed273528ca
FirstWisdom <jason.zelaya21@yahoo.com>
2019-10-06 18:25:02 -0700

    Axes scales and creation complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,4 @@
 // **************************CANVAS SET UP**************************************
-
 // Create the margins
 var margin = {left: 100, right: 20, top: 20, bottom: 100};
 
@@ -37,21 +36,22 @@ d3.csv("./assets/data/data.csv", function(error, data){
   console.log(data);
 
   // Parse the data and convert everything within the forEach function to numbers
+    // Moe = Margin of error
   data.forEach(function(d){
     d.age = +d.age;
-    d.ageMoe = +d.ageMoe;
-    d.healthcare = +d.healthcare;
+    d.ageMoe = +d.ageMoe; // margin of error
+    d.healthcare = +d.healthcare; // average
     d.healthcareHigh = +d.healthcareHigh;
     d.healthcareLow = +d.healthcareLow;
     d.id = +d.id;
     d.income = +d.income;
-    d.incomeMoe = +d.incomeMoe;
-    d.obesity = +d.obesity;
+    d.incomeMoe = +d.incomeMoe; // margin of error
+    d.obesity = +d.obesity; // average
     d.obesityHigh = +d.obesityHigh;
     d.obesityLow = +d.obesityLow;
     d.poverty = +d.poverty;
-    d.povertyMoe = +d.povertyMoe;
-    d.smokes = +d.smokes;
+    d.povertyMoe = +d.povertyMoe; //margin of error
+    d.smokes = +d.smokes; // average
     d.smokesHigh = +d.smokesHigh;
     d.smokesLow = +d.smokesLow;
   });
@@ -60,8 +60,37 @@ d3.csv("./assets/data/data.csv", function(error, data){
   console.log(data);
 
 
-
 // *******************************SCALES****************************************
+// Create the x-axis scale
+var xLinearScale = d3.scaleLinear()
+      // Adjust the start and end x-axis ticks to ensure the circles do not
+        // overlap the edges of the chart
+      .domain([d3.min(data, d => d.poverty) * 1.8,
+        d3.max(data, d => d.poverty) * 1.8])
+      // Enable use of the entire width of the chart
+      .range([0, chartWidth]);
+
+// Create the y-axis scale
+var yLinearScale = d3.scaleLinear()
+      // Highest tick is always the highest healthcare value in the data
+      .domain([0, d3.max(data, d => d.healthcare)])
+      // Move the 0 value to the bottom of the visualization's y-axis
+      .range([chartHeight, 0]);
+
+// *********************************AXES****************************************
+// Axis generators
+var xAxis = d3.axisBottom(xLinearScale);
+var yAxis = d3.axisLeft(yLinearScale);
+
+// Create the chart axes groups
+chart.append("g")
+  .classed("x-axis", true)
+  // Shift the axis to the bottom of the chart
+  .attr("transform", `translate(0, ${chartHeight})`)
+  .call(xAxis);
+
+chart.append("g")
+  .call("yAxis");
 
 
 
