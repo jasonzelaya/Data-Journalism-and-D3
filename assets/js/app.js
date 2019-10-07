384e6f0e7443b4ac0041b60147480907bead0385
FirstWisdom <jason.zelaya21@yahoo.com>
2019-10-06 18:48:03 -0700

    Append the initial circles and change the data.csv path for loading
    Change the path to load data.csv to the GitHub Page path.

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,7 +28,7 @@ var chart = svg.append("g")
 // *********************************DATA****************************************
 
 // Retrieve/load the data and execute everything within the function
-d3.csv("./assets/data/data.csv", function(error, data){
+d3.csv("https://jasonzelaya.github.io/Data-Journalism-and-D3/assets/data/data.csv", function(error, data){
   // Check for errors
   if (error) throw (error);
 
@@ -61,38 +61,47 @@ d3.csv("./assets/data/data.csv", function(error, data){
 
 
 // *******************************SCALES****************************************
-// Create the x-axis scale
-var xLinearScale = d3.scaleLinear()
-      // Adjust the start and end x-axis ticks to ensure the circles do not
-        // overlap the edges of the chart
-      .domain([d3.min(data, d => d.poverty) * 1.8,
-        d3.max(data, d => d.poverty) * 1.8])
-      // Enable use of the entire width of the chart
-      .range([0, chartWidth]);
+  // Create the x-axis scale
+  var xLinearScale = d3.scaleLinear()
+        // Adjust the start and end x-axis ticks to ensure the circles do not
+          // overlap the edges of the chart
+        .domain([d3.min(data, d => d.poverty) * 1.8,
+          d3.max(data, d => d.poverty) * 1.8])
+          // Enable use of the entire width of the chart
+          .range([0, chartWidth]);
 
-// Create the y-axis scale
-var yLinearScale = d3.scaleLinear()
-      // Highest tick is always the highest healthcare value in the data
-      .domain([0, d3.max(data, d => d.healthcare)])
-      // Move the 0 value to the bottom of the visualization's y-axis
-      .range([chartHeight, 0]);
+  // Create the y-axis scale
+  var yLinearScale = d3.scaleLinear()
+        // Highest tick is always the highest healthcare value in the data
+        .domain([0, d3.max(data, d => d.healthcare)])
+        // Move the 0 value to the bottom of the visualization's y-axis
+        .range([chartHeight, 0]);
 
 // *********************************AXES****************************************
-// Axis generators
-var xAxis = d3.axisBottom(xLinearScale);
-var yAxis = d3.axisLeft(yLinearScale);
+  // Axis generators
+  var xAxis = d3.axisBottom(xLinearScale);
+  var yAxis = d3.axisLeft(yLinearScale);
 
-// Create the chart axes groups
-chart.append("g")
-  .classed("x-axis", true)
-  // Shift the axis to the bottom of the chart
-  .attr("transform", `translate(0, ${chartHeight})`)
-  .call(xAxis);
+  // Append the chart axes groups
+  chart.append("g")
+    .classed("x-axis", true)
+    // Shift the axis to the bottom of the chart
+    .attr("transform", `translate(0, ${chartHeight})`)
+    .call(xAxis);
 
-chart.append("g")
-  .call("yAxis");
+  chart.append("g")
+    .call("yAxis");
 
-
+// ******************************UPDATE PATTERN*********************************
+  // Append circles
+  var circles = chart.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+          .attr("cx", d => xLinearScale(d.poverty))
+          .attr("cy", d => yLinearScale(d.healthcare))
+          .attr("r", 5)
+          .attr("fill", "blue")
 
 
 
